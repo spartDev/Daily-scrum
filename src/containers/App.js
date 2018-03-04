779865b2150d59e3cbb5c6bced6dd85df1229f38
Thomas Roux <troux@worldia.com>
2018-03-04 15:36:12 +0100

    feat(button-timer): create button timer

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
-import { Button } from '../components';
+import { Button, ButtonTimer } from '../components';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Container>
         <header role="banner">
           <h1>Stand-up time !</h1>
         </header>
-        <main role="main">
-          content
-        </main>
+        <Main role="main">
+          <ButtonTimer>||</ButtonTimer>
+        </Main>
         <footer>
           <Button>Next</Button>
         </footer>
-      </div>
+      </Container>
     );
   }
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`
+const Main = styled.main`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
 
 export default App;
